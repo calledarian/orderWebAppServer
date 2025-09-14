c31b55fc84b63efe6511d44139fd179ef9cc5a7d
Arian <ariankhadem4@gmail.com>
2025-09-14 18:06:37 +0700

    add module

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TelegramController } from './telegram.controller';
 import { TelegramService } from './telegram.service';
+import { BlockedUsersService } from './services/blocked-users.service';
+import { OrderStateService } from './services/order-state.service';
+import { CallbackHandlerService } from './services/callback-handler.service';
 
 @Module({
   controllers: [TelegramController],
-  providers: [TelegramService],
+  providers: [
+    TelegramService,
+    BlockedUsersService,
+    OrderStateService,
+    CallbackHandlerService,
+  ],
   exports: [TelegramService],
 })
-export class TelegramModule {}
+export class TelegramModule { }
